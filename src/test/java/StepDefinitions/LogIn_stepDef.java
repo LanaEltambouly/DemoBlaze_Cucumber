8e74f28183f57b45ca5b822d6d8e607153f69f4c
Lana Eltambouly <lanaeltambouly@gmail.com>
2025-11-26 02:27:14 +0200

    Base

--- src/test/java/StepDefinitions/LogIn_stepDef.java
+++ src/test/java/StepDefinitions/LogIn_stepDef.java
@@ -1,4 +1,7 @@
 package StepDefinitions;
 
-public class LogIn_stepDef {
+public class LogIn_stepDef {         //no inheritance ashan hooks mn3mlsh mno instance tany w y7sal infinite loop
+                                     //create object mn kol haga
+                                     //pass el driver bel static hooks (Hooks.driver)
+
 }
